8237c1ef86d82a437b914e95154dd1dc4dbe9742
ariva00 <38658922+ariva00@users.noreply.github.com>
2020-11-17 18:02:48 +0100

    Tree test file

---
+++ Prolog/tree.pl
@@ -0,0 +1,28 @@
+%%%% -*- Mode: prolog -*-
+
+graph(gregor).
+
+vertex(gregor, a).
+vertex(gregor, b).
+vertex(gregor, c).
+vertex(gregor, d).
+vertex(gregor, e).
+vertex(gregor, f).
+vertex(gregor, g).
+vertex(gregor, h).
+vertex(gregor, i).
+
+arc(gregor, a, b, 4).
+arc(gregor, a, h, 8).
+arc(gregor, b, c, 8).
+arc(gregor, b, h, 11).
+arc(gregor, c, d, 7).
+arc(gregor, c, f, 4).
+arc(gregor, c, i, 2).
+arc(gregor, d, e, 9).
+arc(gregor, d, f, 14).
+arc(gregor, e, f, 10).
+arc(gregor, f, g, 2).
+arc(gregor, g, h, 1).
+arc(gregor, g, i, 6).
+arc(gregor, h, i, 7).
