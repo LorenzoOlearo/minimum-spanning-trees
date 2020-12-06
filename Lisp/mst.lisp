c77f5d95c01f2a4dd3d94fc0b640a10895b3f8e3
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2020-12-06 16:56:20 +0100

    Added new-arc, various tests

--- Lisp/mst.lisp
+++ Lisp/mst.lisp
@@ -1,3 +1,34 @@
+;; DEBUG ONLY REMOVE BEFORE RELEASE
+(new-graph 'greg)
+
+(new-vertex 'greg 'a)
+(new-vertex 'greg 'b)
+(new-vertex 'greg 'c)
+(new-vertex 'greg 'd)
+(new-vertex 'greg 'e)
+(new-vertex 'greg 'f)
+(new-vertex 'greg 'g)
+(new-vertex 'greg 'h)
+(new-vertex 'greg 'i)
+
+(new-arc 'greg 'b 'a 4)
+(new-arc 'greg 'h 'a 8)
+(new-arc 'greg 'b 'h 11)
+(new-arc 'greg 'i 'h 7)
+(new-arc 'greg 'g 'h 1)
+(new-arc 'greg 'c 'b 8)
+(new-arc 'greg 'c 'i 2)
+(new-arc 'greg 'g 'i 6)
+(new-arc 'greg 'f 'g 2)
+(new-arc 'greg 'f 'c 4)
+(new-arc 'greg 'e 'f 10)
+(new-arc 'greg 'd 'f 14)
+(new-arc 'greg 'd 'c 7)
+(new-arc 'greg 'e 'd )
+;; DEBUG ONLY REMOVE BEFORE RELEASE
+
+
+
 (defparameter *vertices* (make-hash-table :test #'equal))
 (defparameter *arcs* (make-hash-table :test #'equal))
 (defparameter *graphs* (make-hash-table :test #'equal))
@@ -6,13 +37,10 @@
 (defparameter *previous* (make-hash-table :test #'equal))
 
 
-;; TODO
-;; (setf (gethash ’il-mio-grafettino *graphs*) ...)
-
-
 
 (defun is-graph (graph-id)
   (gethash graph-id *graphs*))
+
 
 
 (defun new-graph (graph-id)
@@ -20,7 +48,40 @@
       (setf (gethash graph-id *graphs*) graph-id)))
 
 
+
 (defun new-vertex (graph-id vertex-id)
   (setf (gethash (list 'vertex graph-id vertex-id)
                  *vertices*)
-  (list 'vertex graph-id vertex-id)))
+        (list 'vertex graph-id vertex-id)))
+
+
+
+;; Returns a list containing all the vertices in a given graph rapresented by
+;; its graph-id
+;; TODO
+
+(defun graph-vertices (graph-id)
+  (maphash #'((lambda (graph-id vertex-id)
+                (list 'vertex graph-id vertex-id)))
+           *vertices*))
+
+
+
+;; TEMP DEMO
+;; What graph-vertices should do.
+
+(defun hash-keys (hash-table)
+  (loop for key being the hash-keys of hash-table collect key))
+
+
+
+;; The function new-arc creates an entry in the hash table rapresenting a new
+;; arc between two vertices using the following notation:
+;; (arc graph-id vertex-id-destination vertex-id-source weight)
+;; Note that the parameter rapresenting the arc's weight is optional, its
+;; default value is set to 1.
+
+(defun new-arc (graph-id vertex-id-dest vertex-id-source &optional (weight 1))
+  (setf (gethash (list 'arc graph-id vertex-id-dest vertex-id-source weight)
+                 *arcs*)
+        (list 'arc graph-id vertex-id-dest vertex-id-source weight)))
